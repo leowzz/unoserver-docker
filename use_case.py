d0433e7231c22240a907967073f3d49e4ec49909
leo <3181538941@qq.com>
2024-08-22 18:12:01 +0800

    add req

--- use_case.py
+++ use_case.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-
+# 此用例未实际测试 仅供参考
 from unoserver.client import UnoClient
-from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi import FastAPI, HTTPException, UploadFile, Response
 from loguru import logger
 
 UNOSERVER_ENDPOINT = "http://127.0.0.1"
@@ -12,10 +12,12 @@ app = FastAPI()
 @app.post("/convert_ppt_to_pdf/")
 async def conv_ppt_to_pdf(file: UploadFile):
     """Convert ppt to pdf"""
+    # 此用例未实际测试 仅供参考
     logger.info(f"ppt parse start:{file.filename=}")
     in_file_bytes = await file.read()
     try:
         uno_client = UnoClient(UNOSERVER_ENDPOINT, port="2003")
+        # 此用例未实际测试 仅供参考
         pdf_file_bytes = uno_client.convert(indata=in_file_bytes, convert_to='pdf')
         logger.info(f"ppt parse completed:{file.filename=}")
     except Exception as e:
